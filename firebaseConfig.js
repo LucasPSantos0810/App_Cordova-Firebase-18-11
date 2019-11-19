a2e0b9c3990fc057961bfb207bb6fdf739490c8d
LucasPSantos0810 <lucasp.santos0810@gmail.com>
2019-11-18 22:41:03 -0300

    aula-18-11

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+var fbConfig = {
+    apiKey: "AIzaSyAXnNIIl5jW-ygUuwBaVsjIceBN0ygKVcE",
+    authDomain: "fir-d89a4.firebaseapp.com",
+    databaseURL: "https://fir-d89a4.firebaseio.com",
+    projectId: "fir-d89a4",
+    storageBucket: "fir-d89a4.appspot.com",
+    messagingSenderId: "17203033650",
+    appId: "1:17203033650:web:03f80dcdc973c5a6fd6e2b",
+    measurementId: "G-2XRV97NY8F"
+  };
+    
